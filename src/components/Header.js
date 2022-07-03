77b914f2ff475a410eb5c16c1c319d86dfa5694f
Joseph <jsltran@gmail.com>
2022-07-03 14:20:42 -0700

    refactor code

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import react from "react";
+
+function Header({ onDarkModeClick, isDarkMode }) {
+    return(
+        <div>
+            <header>
+                <h2>Shopster</h2>
+                <button onClick={onDarkModeClick}>
+                {isDarkMode ? "Dark" : "Light"} Mode
+                </button>
+            </header>
+        </div>
+    )
+};
+
+export default Header;
